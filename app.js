8b52a2ee067cd79237423f884e70a5bca738d64d
Gordon Lau Wai chung <gordonlauwaichung@gmail.com>
2017-11-30 16:35:11 +0800

    Changed the text

--- app.js
+++ app.js
@@ -2,6 +2,6 @@
 var http = require('http');
 http.createServer(function (req, res) {
    res.writeHead(200, {'Content-Type': 'text/plain'});
-   res.end('Hello World\n');
+   res.end('Hello World to all of the IT dogs.\n');
 }).listen(8080,'localhost');
 console.log("Server running at http://localhost:8080/")
